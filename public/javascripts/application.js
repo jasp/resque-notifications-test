ba10b05a5afedc7ee51d3ba6aa50c60f42bd4735
Jakob Skov-Pedersen <jasp@thecrypt.dk>
2011-03-04 00:06:22 +0100

    Use html5 boilerplate.
    More info: http://html5boilerplate.com/

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// usage: log('inside coolFunc',this,arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function(){
+  log.history = log.history || [];   // store logs to an array for reference
+  log.history.push(arguments);
+  if(this.console){
+    console.log( Array.prototype.slice.call(arguments) );
+  }
+};
