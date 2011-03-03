656de4bea64fa8a962c07d671b3ec86d89dd5a15
Jakob Skov-Pedersen <jasp@thecrypt.dk>
2011-03-03 20:46:49 +0100

    Test of notification with Resque

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notifier::Application.config.secret_token = '7cfad882417ee76c14d056253f6c9f1b236087a80fdcb082db874479cf8826974fd3dfdc1ac81284a35c6499d4227ef09f2a268c7c76fa55736c7e2596f4c941'
